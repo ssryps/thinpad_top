85b135f9680f126eb73588ee836fe16c4d3cd0b0
eric <zhengly123@foxmail.com>
2018-11-01 20:54:25 +0800

    Fix: first 44 test passed
    A MFLO is subsititued by a MTLO by mistake in ex.v.

--- thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/xsim_webtalk.tcl
+++ thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Nov  1 17:04:00 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Nov  1 20:52:10 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2188600" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,19 +14,19 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "a73b1e7b-5928-4a89-8515-a8522642b6e7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "1e0777dd008c45fcbaffd6b18f2c35e9" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "118" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "16" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 16.04.5 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-6200U CPU @ 2.30GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "1275.608 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "2699.997 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "10 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "3 ms" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "129740_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3576426504 -regid "" -xml /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "3.88_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "153352_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 346065463 -regid "" -xml /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/eric/Work/jy/thinpad_top/thinpad_top.sim/sim_1/behav/xsim/xsim.dir/closemips_testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
